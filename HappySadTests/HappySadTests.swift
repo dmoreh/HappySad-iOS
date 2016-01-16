c9784db1331ab3eaeb14fa05dd589390f6cc692c
Adam Reis <adamhreis@gmail.com>
2016-01-16 03:31:01 -0800

    Initial Commit

---
+++ HappySadTests/HappySadTests.swift
@@ -0,0 +1,36 @@
+//
+//  HappySadTests.swift
+//  HappySadTests
+//
+//  Created by Adam Reis on 1/16/16.
+//  Copyright © 2016 Adam Reis. All rights reserved.
+//
+
+import XCTest
+@testable import HappySad
+
+class HappySadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
